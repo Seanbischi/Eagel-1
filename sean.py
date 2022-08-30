8d2e0c2677eb5b5bd3197b753d7140ee49e9a7ff
Seanbischi <111909592+Seanbischi@users.noreply.github.com>
2022-08-30 20:10:42 +0200

    Python code
    First Version of Python code written by S.B

---
+++ sean.py
@@ -0,0 +1,17 @@
+def main():
+    A = 5
+    B = 4
+    print(f'your affreg in computer sciens was')
+    print(f'{A+B}/2')
+
+def main():
+    A = 3.5
+    B = 4.5
+    C = 5
+    print(f'your avreg in computer sciens was:')
+    print(f'{A+B+C}/3')
+def main():
+    print(f'thank you for attedging our class') 
+
+if __name__ == '__main__':
+    main()
